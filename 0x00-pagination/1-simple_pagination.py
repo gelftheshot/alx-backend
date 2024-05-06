0b55e06fe972f026e3389c49a09cf2efb93bf68e
Leon Gebre <gelf.learing@gmail.com>
2024-05-06 22:12:50 +0300

    fixing typeing error

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 """ the first steps toward implementing pangation """
-import typing
+from typing import List, Tuple
 import csv
 import math
 
 
-def index_range(page: int, page_size: int) -> typing.Tuple[int, int]:
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     ''' Return tuple containing pagination start index and end index. '''
     return ((page_size * (page - 1)), page_size * page)
 
