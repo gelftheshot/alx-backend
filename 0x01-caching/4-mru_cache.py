425a720999648be843e215a1b770ec161a66d4e3
Leon Gebre <gelf.learing@gmail.com>
2024-05-09 18:12:47 +0300

    mru caching system

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+''' MRU Caching
+    Create a class MRUCache that inherits from BaseCaching
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    ''' 
+        An MRU Cache.
+    '''
+
+    def __init__(self):
+        ''' Initialize class instance. '''
+        super().__init__()
+        self.key_queue = []
+
+    def put(self, key, item):
+        '''
+            Add key/value pair to cache data.
+        '''
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.key_queue:
+                self.key_queue.append(key)
+            else:
+                self.key_queue.append(self.key_queue.pop(self.key_queue.index(key)))
+            if len(self.key_queue) > BaseCaching.MAX_ITEMS:
+                discard = self.key_queue.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+
+    def get(self, key):
+        ''' Return value stored in `key` key of cache.
+            If key is None or does not exist in cache, return None. '''
+        if key is not None and key in self.cache_data:
+            self.key_queue.append(self.key_queue.pop(self.key_queue.index(key)))
+            return self.cache_data[key]
+        return None
