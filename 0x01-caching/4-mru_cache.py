cad510a52fa003e6e6a425b3d9fa747d85343376
Leon Gebre <gelf.learing@gmail.com>
2024-05-10 19:49:17 +0300

    fixing pycodestyle

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -7,34 +7,40 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class MRUCache(BaseCaching):
-    ''' 
-        An MRU Cache.
+    '''
+    An MRU Cache.
     '''
 
     def __init__(self):
-        ''' Initialize class instance. '''
+        ''' constructor '''
         super().__init__()
         self.key_queue = []
 
     def put(self, key, item):
         '''
-            Add key/value pair to cache data.
+        Add key/value pair to cache data.
         '''
         if key is not None and item is not None:
             self.cache_data[key] = item
             if key not in self.key_queue:
                 self.key_queue.append(key)
             else:
-                self.key_queue.append(self.key_queue.pop(self.key_queue.index(key)))
+                self.key_queue.append(
+                    self.key_queue.pop(self.key_queue.index(key))
+                )
             if len(self.key_queue) > BaseCaching.MAX_ITEMS:
-                discard = self.key_queue.pop(-2)
+                discard = self.key_queue.pop(-1)
                 del self.cache_data[discard]
                 print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
-        ''' Return value stored in `key` key of cache.
-            If key is None or does not exist in cache, return None. '''
+        '''
+        Return value stored in `key` key of cache.
+        If key is None or does not exist in cache, return None.
+        '''
         if key is not None and key in self.cache_data:
-            self.key_queue.append(self.key_queue.pop(self.key_queue.index(key)))
+            self.key_queue.append(
+                self.key_queue.pop(self.key_queue.index(key))
+            )
             return self.cache_data[key]
         return None
