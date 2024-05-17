9588de9b2c75aad81173f16e6dd57639c68a9116
Leon Gebre <gelf.learing@gmail.com>
2024-05-17 15:19:40 +0300

    updating the way of return

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,6 +3,7 @@
 
 from flask import Flask, render_template
 from flask_babel import Babel
+from os import getenv
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -22,5 +23,15 @@ def main() -> str:
     """ A function to return a template """
     return render_template('1-index.html')
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return: 1-index.html
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
