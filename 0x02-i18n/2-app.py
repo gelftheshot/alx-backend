945e877a40bd2c9bc40f7d2b51d926bb5fb8a3f3
Leon Gebre <gelf.learing@gmail.com>
2024-05-17 19:51:33 +0300

    true

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -2,7 +2,7 @@
 """
 A Basic flask application
 """
-from flask import Flask,request
+from flask import Flask, request
 from flask import render_template
 from flask_babel import Babel
 
@@ -17,25 +17,23 @@ class Config(object):
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-babel = Babel(app, locale_selector=get_locale)
+babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> str:
+def get_locale():
     """
     Gets locale from request object
     """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return request.accept_languages.best_match(Config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
-def index() -> str:
+def index():
     """
     Renders a basic html template
     """
     return render_template('2-index.html')
-
 
 
 if __name__ == '__main__':
