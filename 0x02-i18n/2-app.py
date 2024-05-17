6136165cb01ba9e44c57f6c76b9cdd6b36889aac
Leon Gebre <gelf.learing@gmail.com>
2024-05-17 20:04:42 +0300

    trying to find out what si wrongg

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,45 +1,42 @@
 #!/usr/bin/env python3
-"""
-A Basic flask application
-"""
-from flask import Flask
-from flask import request
-from flask import render_template
+""" Route module for the API - Get locale from request"""
+
+
+from flask import Flask, request, render_template
 from flask_babel import Babel
+from os import getenv
+
+app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config(object):
-    """
-    Application configuration class
-    """
+    """ Setup - Babel configuration """
     LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-# Instantiate the application object
-app = Flask(__name__)
-app.config.from_object(Config)
-
-# Wrap the application with Babel
-babel = Babel(app)
+# set the above class object as the configuration for the app
+app.config.from_object('2-app.Config')
 
 
-@babel.localeselector
-def get_locale() -> str:
-    """
-    Gets locale from request object
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-@app.route('/', strict_slashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index() -> str:
-    """
-    Renders a basic html template
+    """ GET /
+    Return: 2-index.html
     """
     return render_template('2-index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+@babel.localeselector
+def get_locale() -> str:
+    """ Determines best match for supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
