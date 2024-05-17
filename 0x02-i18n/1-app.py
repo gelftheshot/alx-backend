36e02a012c2bc302d9685506d503df6a6b282b37
Leon Gebre <gelf.learing@gmail.com>
2024-05-17 14:39:09 +0300

    addin some very not importat

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,23 +3,22 @@
 
 from flask import Flask, render_template
 from flask_babel import Babel
-from datetime import datetime
-import pytz
 
 app = Flask(__name__)
+babel = Babel(app)
 
-class Config():
+
+class Config(object):
     """ The class to configure Babel """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
-app.config.from_object(Config)
+app.config.from_object('1-app.Config')
 
-babel = Babel(app)
 
 @app.route('/')
-def main():
+def main() -> str:
     """ A function to return a template """
     return render_template('1-index.html')
 
