57476a2b40025a68dbc79d0652b5bf8fbf9e3a5c
Leon Gebre <gelf.learing@gmail.com>
2024-05-17 14:22:00 +0300

    trying with out clue

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Using Babel for the first time in my life """
+
+from flask import Flask, render_template
+from flask_babel import Babel
+from datetime import datetime
+import pytz
+
+class Config():
+    """ The class to configure Babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    TIMEZONE = pytz.timezone("UTC")
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+@app.route('/')
+def main():
+    """ A function to return a template """
+    current_time = datetime.now(Config.TIMEZONE)
+    return render_template('1-index.html', current_time=current_time)
+
+if __name__ == '__main__':
+    app.run()
