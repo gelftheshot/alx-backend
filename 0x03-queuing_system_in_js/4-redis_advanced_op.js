70e20988e1278e056ec97bab4bbf58675233159f
Leon Gebre <gelf.learing@gmail.com>
2024-06-10 20:05:57 +0300

    using hset to set a dictionary to a value in redis

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,13 @@
+import { createClient } from "redis";
+import { print } from "redis";
+const client = createClient()
+
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
+
+client.hSet('HolbertonSchools', 'Portland', 50, print);
+client.hSet('HolbertonSchools', 'Seattle', 80, print);
+client.hSet('HolbertonSchools', 'New York', 20, print);
+client.hSet('HolbertonSchools', 'Bogota', 20, print);
+client.hSet('HolbertonSchools', 'Cali', 40, print);
+client.hSet('HolbertonSchools', 'Paris', 2, print);
