25456db2d10a97713f0d60f50ed8038d13a2d68f
Leon Gebre <gelf.learing@gmail.com>
2024-06-10 15:47:29 +0300

    i should use redis.print instead of  console.lot

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -1,12 +1,12 @@
 import { createClient } from "redis";
-
+import { print } from "redis";
 const client = createClient();
 
 client.on('error', (err) => console.log('Redis client not connected to the server:', err));
 client.on('connect', () => console.log('Redis client connected to the server'));
 
 const setNewSchool = (schoolName, value) => {
-  client.set(schoolName, value, redis.print);
+  client.set(schoolName, value, print);
 };
 
 const displaySchoolValue = (schoolName) => {
