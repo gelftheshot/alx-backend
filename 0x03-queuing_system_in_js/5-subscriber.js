219fc222f580726281d9b64fced46b721df58891
Leon Gebre <gelf.learing@gmail.com>
2024-06-11 00:00:46 +0300

    the subscriber must be duplicated inead of other thing

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,8 +1,16 @@
 import { createClient } from "redis";
 
-
 (async () => {
-  client = createClient()
+  const client = createClient();
+  
   client.on('error', (err) => console.log('Redis client not connected to the server:', err));
   client.on('connect', () => console.log('Redis client connected to the server'));
-})
+
+  const subscriber = client.duplicate();
+
+  subscriber.on("message", (channel, message) => {
+    console.log(`Received the following message from ${channel}: ${message}`);
+  });
+
+  await subscriber.subscribe('article');
+})();
