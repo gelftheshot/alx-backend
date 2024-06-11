b93b8ad3bd26febbb7fa9e6a45b09f79f091229d
Leon Gebre <gelf.learing@gmail.com>
2024-06-11 11:17:16 +0300

    immporting kue and assigning it in to a varable

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,15 +1,17 @@
-import 'kue'
+import kue from 'kue';
 
 const jobData = {
   phoneNumber: '11223344',
   message: 'i love you',
 }
 
-const push_notification_code = kue.createQueue();
+
+const push_notification_code = kue.createQueue()
 
 const job = push_notification_code.create('Myjobtype', jobData).save((error) => {
   if (error) {
-    console.log('Notification job failed')
+    console.log('Notification job failed');
+  } else {
+    console.log('Notification job created:', job.id);
   }
-  console.log('Notification job created:', job.id)
 });
