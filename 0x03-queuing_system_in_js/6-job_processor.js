9b71598ef93cced34ce8027a8bca2ff2a05cc983
Leon Gebre <gelf.learing@gmail.com>
2024-06-11 11:26:29 +0300

    creating new type of process of kue that i made

---
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -0,0 +1,11 @@
+import kue from 'kue'
+
+const Kue = kue.createQueue()
+
+const sendNotification = (phoneNumber, message) => {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`)
+}
+
+Kue.process('Myjobtype', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message)
+});
