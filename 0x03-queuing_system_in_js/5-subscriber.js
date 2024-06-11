8da4de795af4b366d46f3b821b3e74e7b88ff777
Leon Gebre <gelf.learing@gmail.com>
2024-06-11 10:38:15 +0300

    subscring to holberton schoo and unsbscribing if message is kill server

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,16 +1,18 @@
 import { createClient } from "redis";
 
-(async () => {
-  const client = createClient();
-  
-  client.on('error', (err) => console.log('Redis client not connected to the server:', err));
-  client.on('connect', () => console.log('Redis client connected to the server'));
+const client = createClient();
 
-  const subscriber = client.duplicate();
+client.on('error', (err) => console.log('Redis client not connected to the server:', err));
+client.on('connect', () => console.log('Redis client connected to the server'));
 
-  subscriber.on("message", (channel, message) => {
-    console.log(`Received the following message from ${channel}: ${message}`);
-  });
+const subscriber = client.duplicate();
 
-  await subscriber.subscribe('article');
-})();
+subscriber.on("message", (channel, message) => {
+  console.log(`Received the following message from ${channel}: ${message}`);
+  if (message === 'KILL_SERVER') {
+    subscriber.unsubscribe();
+    subscriber.quit();
+  }
+});
+
+subscriber.subscribe('holberton school channel');
