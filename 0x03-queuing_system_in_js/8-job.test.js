f6cd9b2adaf88c329483d3c5daa51683936cbd41
Leon Gebre <gelf.learing@gmail.com>
2024-06-12 13:19:52 +0300

    mocking up the process of the job for the process

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -1,22 +1,44 @@
 import kue from 'kue';
 import createPushNotificationsJobs from './8-job.js';
 import { expect } from 'chai';
+import sinon from 'sinon';
 
 describe('createPushNotificationsJobs', () => {
   it('should throw an error if jobs is not an array', () => {
     expect(() => createPushNotificationsJobs('not an array', kue.createQueue())).to.throw(Error, 'Jobs is not an array');
   });
 
-  it('should create jobs in the queue for each item in jobs', () => {
-    const jobs = [{ id: 1 }, { id: 2 }];
-    const queue = kue.createQueue();
+it('should create jobs in the queue for each item in jobs', (done) => {
+  const jobs = [{ id: 1 }, { id: 2 }];
+  const queue = kue.createQueue();
 
-    createPushNotificationsJobs(jobs, queue);
+  // Stub the queue.create method
+  const createStub = sinon.stub(queue, 'create');
 
-    expect(queue.testMode.jobs.length).to.equal(jobs.length);
-    queue.testMode.jobs.forEach((job, index) => {
-      expect(job.type).to.equal('push_notification_code_3');
-      expect(job.data).to.equal(jobs[index]);
-    });
+  // Simulate job processing
+  createStub.callsFake((type, data) => {
+    return {
+      save: (callback) => {
+        callback(null);  // Simulate successful job creation
+      },
+      on: (event, callback) => {
+        if (event === 'complete') {
+          callback();  // Simulate job completion
+        }
+      },
+      id: data.id,
+    };
   });
+
+  createPushNotificationsJobs(jobs, queue);
+
+  setTimeout(() => {
+    expect(createStub.callCount).to.equal(jobs.length);
+    createStub.args.forEach((args, index) => {
+      expect(args[0]).to.equal('push_notification_code_3');
+      expect(args[1]).to.equal(jobs[index]);
+    });
+    done();
+  }, 500);
+});
 });
